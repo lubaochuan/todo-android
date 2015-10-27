2623cb1059af88a659767b043c2227b6f8241cae
lubaochuan <lubaochuan@gmail.com>
2015-10-27 15:26:05 -0500

    Add list of items

--- app/src/main/java/org/createdtolearn/todo_andorid/MainActivity.java
+++ app/src/main/java/org/createdtolearn/todo_andorid/MainActivity.java
@@ -1,13 +1,28 @@
 package org.createdtolearn.todo_andorid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
     }
 }
